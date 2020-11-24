bc4c5428ca3ac7419a021d344a5d7acc08f8c74e
niklasriebesell <niklas-riebesell@outlook.de>
2020-11-24 11:59:00 +0100

    first Commit

---
+++ src/styles/globalstyle.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    font-size: 112.5%;
+    font-family: sans-serif;
+  }
+  `;
