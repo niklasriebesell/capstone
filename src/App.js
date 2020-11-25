30af715a82374b081d26f29753abb535d0d82f5f
niklasriebesell <niklas-riebesell@outlook.de>
2020-11-25 11:45:15 +0100

    Teamstruktur

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 /** @format */
-
-import "./App.css";
+import GlobalStyle from './styles/globalstyle'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
