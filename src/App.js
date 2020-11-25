8a18de4f606c3680c46e2e0f42ce8b56bfb12bc0
niklasriebesell <niklas-riebesell@outlook.de>
2020-11-25 12:44:42 +0100

    added Header and Footer without Styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 /** @format */
 import GlobalStyle from './styles/globalstyle'
-import './App.css'
+import Header from './Components/header'
+import Footer from './Components/footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+function Footer() {
+  return (
+    <footer>
+      <button>Back</button>
+      <button>Home</button>
+    </footer>
+  )
+}
+
+export default Footer
