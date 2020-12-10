9203709ebbf96a70248cf9017e4d8f2c4017b7a0
niklasriebesell <niklas-riebesell@outlook.de>
2020-12-10 13:07:21 +0100

    added function on data

--- client/src/Leagues/NFL/InjuredPlayerCard.js
+++ client/src/Leagues/NFL/InjuredPlayerCard.js
@@ -3,33 +3,58 @@ import styled from 'styled-components/macro'
 import getNflInjuryData from '../../Api/fetchDataNfl'
 
 export default function InjuredPlayerCard() {
+  const [teams, setTeams] = useState([])
+  const [selectedTeam, setSelectedTeam] = useState('')
   const [players, setPlayers] = useState([])
 
   useEffect(
     () =>
       getNflInjuryData()
-        .then((playersFromApi) => setPlayers(playersFromApi))
+        .then((teams) => {
+          const teamKeys = Object.keys(teams)
+          const players = []
+          teamKeys.forEach((teamKey) =>
+            teams[teamKey].forEach((player) => players.push(player))
+          )
+          setTeams(teamKeys)
+          setPlayers(players)
+        })
         .catch((error) => console.error(error)),
     []
   )
 
-  /*Object.keys(players).map((key) => {
-    const player = players[key];
+  /*Object.keys(teams).map((teamKey) => {
+    const players = teams[teamKey];
   })*/
 
   return (
     <>
-      {players &&
-        players?.map((player, index) => (
-          <Div key={index} style={{ color: 'white' }}>
-            <p>Name:{player.playerName}</p>
-            <p>Team:{player.team}</p>
-            <p>Position:{player.position}</p>
-            <p>Injury:{player.injury}</p>
-            <p>Status:{player.gameStatus}</p>
-            <p>lastupdated:{player.lastUpdate}</p>
-          </Div>
+      {teams &&
+        teams.map((team) => (
+          <button
+            type="button"
+            key={team}
+            onClick={() => setSelectedTeam(team)}
+          >
+            {team}
+          </button>
         ))}
+      {players &&
+        players?.map(
+          (player, index) =>
+            selectedTeam &&
+            selectedTeam.length > 0 &&
+            player.team === selectedTeam && (
+              <Div key={index} style={{ color: 'white' }}>
+                <p>Name:{player.playerName}</p>
+                <p>Team:{player.team}</p>
+                <p>Position:{player.position}</p>
+                <p>Injury:{player.injury}</p>
+                <p>Status:{player.gameStatus}</p>
+                <p>lastupdated:{player.lastUpdate}</p>
+              </Div>
+            )
+        )}
     </>
   )
 }
